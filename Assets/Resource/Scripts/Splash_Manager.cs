fffcd78f889d699efbf70570d3819a6f9275459f
devfcat <devfcat@gmail.com>
2024-08-06 15:29:18 +0900

    [feat] 스플래시 씬 초기화 설정 기능 추가

--- Assets/Resource/Scripts/Splash_Manager.cs
+++ Assets/Resource/Scripts/Splash_Manager.cs
@@ -2,32 +2,46 @@ using UnityEngine;
 using System;
 using System.Runtime.InteropServices;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 /// <summary>
-/// 
+/// 스플래시 화면을 보여준 후 전체화면 변경/메인 프로세스 실행
 /// </summary>
- 
-[Header("스플래시 화면 구성 컴포넌트")]
-public GameObject Splash;
-public float timer;
 
 public class Splash_Manager : MonoBehaviour
 {
-    public Awake()
+    [Header("스플래시 화면 구성 컴포넌트")]
+    public GameObject Splash;
+    public float timer = 0f;
+    [Range(1f,10f)] public float limit_Time;
+
+    public void Awake()
     {
         UnityEngine.Application.runInBackground = true;
         UnityEngine.Application.targetFrameRate = 30;
-        Init();
+        this.Init();
         Splash.SetActive(true);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > limit_Time)
+        {
+            Setting();
+            timer = 0f;
+        }
     }
 
     public void Init()
     {
-        Screen.SetResolution(300f, 400f, false);
+        UnityEngine.Screen.SetResolution(300, 400, false);
+        timer = 0f;
     }
-
     public void Setting()
     {
         Screen.SetResolution(Screen.width, Screen.height, true);
+        SceneManager.LoadSceneAsync("Puddinget");
     }
 }
